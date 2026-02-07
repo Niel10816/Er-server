a74cf2d6f479a97a07f3710024399a706ed1aa0f
Niel10816 <niel10816albaredo@gmail.com>
2026-02-07 16:15:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 import streamlit as st
+import csv
+import os
 
 st.title("Er software")
 
-nome = st.text_input("Inserisci il tuo nome:")
+email = st.text_input("Inserisci la tua email")
 
-if st.button("Saluta"):
-    st.write(f"Ciao {nome}, benvenuto dentro ar software!")
+if email:
+    nome_file = email.replace("@","_").replace(".","_") + ".csv"
+
+    nome = st.text_input("Come ti chiami?")
+    età = st.text_input("Quanti anni hai?")
+    scuola = st.text_input("Che scuola frequenti?")
+
+    if st.button("Sono di sesso maschile"):
+        sesso = ("Maschio")
+    if st.button("Sono di sesso femminile"):
+        sesso = ("Femmina")
+    if st.button("Preferisco non rispondere"):
+        sesso = ("Indefinito")
+        
+    if st.button ("salva"):
+        with open(nome_file, "a", newline="") as f:
+            writer= csv.writer(f)
+            writer.writerow([nome, età, scuola, sesso])
+            st.success("Dati salvati")
+    if os.path.exsists(nome_file):
+        st.write("I tuoi dati salvati:")
+        with open(nome_file, "r") as f:
+            st.text(f.read())
+
