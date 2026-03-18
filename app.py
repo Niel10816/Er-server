a081508b5b77656f0ec4f5f1cfa78fc2111f98d5
Niel10816 <niel10816albaredo@gmail.com>
2026-03-18 17:50:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 from supabase import create_client
-url = "https://kjqncgasirjzilovibat.supabase.co"
+url = "https://hcyuowvrrjccmvcgebaj.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqcW5jZ2FzaXJqemlsb3ZpYmF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NjkyNTgsImV4cCI6MjA4ODA0NTI1OH0.qDVawy4rq8QCO9fP5tJsTSVN8PrgZW5BKwFOWxX2ECI"
 supabase = create_client(url,key)
 st.title("BeginnerCollab")
