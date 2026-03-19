d04290a23230e7894ebaeb3f8b623d818c65363c
Niel10816 <niel10816albaredo@gmail.com>
2026-03-19 19:07:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ genere = st.selectbox("Genere musicale", ["rock", "jazz", "tecno", "pop","classi
 audio_file = st.file_uploader("Carica un audio (mp3)", type=["mp3"])
 
 if st.button("Salva il profilo"):
-
-audio_url = None
+ audio_url = None
 
     if audio_file is not None:
         file_bytes = audio_file.read()
