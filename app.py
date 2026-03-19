3ec5ac7d1e5c9bfe64e1cd0a6a3e32e47d75bba5
Niel10816 <niel10816albaredo@gmail.com>
2026-03-19 19:07:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,9 @@ audio_file = st.file_uploader("Carica un audio (mp3)", type=["mp3"])
 
 if st.button("Salva il profilo"):
  audio_url = None
-
-    if audio_file is not None:
-        file_bytes = audio_file.read()
-        nome_file = f"{nome.lower().replace(' ', '_')}.mp3"
+ if audio_file is not None:
+  file_bytes = audio_file.read()
+  nome_file = f"{nome.lower().replace(' ', '_')}.mp3"
 
     supabase.storage.from_("audio").upload(nome_file, file_bytes, {"upsert": True})
     audio_url = supabase.storage.from_("audio").get_public_url(nome_file)["publicUrl"]
