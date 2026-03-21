41d6058f3b04676d9d7e8656dee2611b9cdf2afc
Niel10816 <niel10816albaredo@gmail.com>
2026-03-21 15:13:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if st.button("Salva il profilo"):
 
         supabase.storage.from_("audio").upload(nome_file, file_bytes)
 
-        audio_url = supabase.storage.from_("audio").get_public_url(nome_file)["publicUrl"]
+        audio_url = supabase.storage.from_("audio").get_public_url(nome_file)
 
     supabase.table("utenti").insert({
         "nome": nome,
