39d049e765b8bb1fdfed8eba546536a39b810d46
Niel10816 <niel10816albaredo@gmail.com>
2026-03-22 18:44:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ audio_file = st.file_uploader("Carica un audio (mp3)", type=["mp3"])
 
 if st.button("Salva il profilo"):
 
-audio_url = None  
+ audio_url = None  
 
 if audio_file is not None:  
     file_bytes = audio_file.read()  
